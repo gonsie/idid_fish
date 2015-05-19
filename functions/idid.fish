4c1c8a1206541568710360c616aa1e211a5e0115
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-05-19 13:04:30 -0400

    idid prev for date adjustments

--- functions/idid.fish
+++ functions/idid.fish
@@ -3,6 +3,7 @@ function idid -d "idid this"
          idid_source
          idid_what $argv[1]
          idid_tag $argv[1]
+         idid_prev $argv
          if test $argv
             set -l __pd (tail -r $IDIDS | grep -m 1 "^[^-;]")
             set -l __td (date "+%a %b %d %Y")
---
+++ functions/idid_prev.fish
@@ -0,0 +1,20 @@
+function idid_prev -d "idid with date adjustment"
+         set __what 0
+         for h in $argv
+            if test $h = "-p"
+               set __what 1
+            else if test $h = "-prev"
+               set __what 1
+            else if test $h = "--prev"
+               set __what 1
+            end
+         end
+         if test $__what -eq 1
+            set -l __pd (tail -r $IDIDS | grep -m 1 "^[^-;]")
+            set -l __td (date -v{$argv[2]} "+%a %b %d %Y")
+            if not test $__pd = $__td
+               date -v{$argv[2]} "+%a %b %d %Y" >> $IDIDS
+            end
+            echo "- $argv[3]" >> $IDIDS
+         end
+end
